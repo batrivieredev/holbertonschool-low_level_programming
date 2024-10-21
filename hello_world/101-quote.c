a6a4fe54490bbf6316de44a13887cf3e66574e6c
batrivieredev <baptiste.rivierefr@gmail.com>
2024-10-21 09:23:02 +0000

    100

--- hello_world/101-quote.c
+++ hello_world/101-quote.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
-#include <unistd.h>
 
 /**
  * main - Entry point
@@ -9,7 +8,6 @@
 
 int main(void)
 {
-	write(STDOUT_FILENO, "and that piece of art is useful", 31);
-	write(STDOUT_FILENO, "\" - Dora Korpar, 2015-10-19\n", 28);
-	return (1);
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
