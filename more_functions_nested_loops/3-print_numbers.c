aadade2f5647613e4bbf9090f5e13a17a26e84ce
batrivieredev <baptiste.rivierefr@gmail.com>
2024-10-28 08:01:20 +0100

    3

---
+++ more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * print_numbers - check the code.
+ *
+ * Return: Nothing
+ */
+void print_numbers(void)
+{
+	int count;
+
+	for (count = 0; count < 10; count++)
+		_putchar(count + '0');
+	_putchar('\n');
+}
