256813dd3ebfd75b83c117c0ce4bbd566866f418
batrivieredev <baptiste.rivierefr@gmail.com>
2024-10-28 08:17:57 +0100

    3

---
+++ pointers_arrays_strings/3-puts.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _puts - check the code
+ * @str: start of string
+ *
+ * Return: Nothing
+ */
+void _puts(char *str)
+{
+	int count = 0;
+
+	while (str[count] != '\0')
+	{
+		_putchar(str[count]);
+		count++;
+	}
+	_putchar('\n');
+}
