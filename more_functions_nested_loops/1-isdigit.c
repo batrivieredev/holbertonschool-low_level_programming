df3c15a7cfaab4a5b452a446356ccb7c4bcc8208
batrivieredev <baptiste.rivierefr@gmail.com>
2024-10-28 08:00:15 +0100

    1,2

---
+++ more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * _isdigit - check the code.
+ * @c: the character to test
+ * Return: 1 if c is a digit.
+ */
+int _isdigit(int c)
+{
+	return (c >= '0' && c <= '9');
+}
---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * mul - check the code.
+ * @a: first number
+ * @b: second number
+ * Return: a times b.
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
