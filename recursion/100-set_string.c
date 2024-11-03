81333626601f8c6c007b658201c1a5cd89115611
batrivieredev <baptiste.rivierefr@gmail.com>
2024-11-04 00:38:52 +0100

    100

---
+++ recursion/100-set_string.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * set_string - check the code
+ * @s: double pointer
+ * @to: size of the array
+ * Return: pointer to first match
+ */
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
