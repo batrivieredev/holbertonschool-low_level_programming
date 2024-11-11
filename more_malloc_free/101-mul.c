2cc98e6a370d775d9285614dea302c424c91e0ae
batrivieredev <baptiste.rivierefr@gmail.com>
2024-11-11 14:24:11 +0100

    101

--- more_malloc_free/101-mul.c
+++ more_malloc_free/101-mul.c
@@ -1,128 +1,101 @@
 #include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+#define ERR_MSG "Error"
 
 /**
- * main - Program that mulplies two positive numbers
- * @argc: Number of argumenets
- * @argv: Muldimensional array of arguments
- * Return: Always 0 (Success)
+ * is_digit - checks if a string contains a non-digit char
+ * @s: string to be evaluated
+ *
+ * Return: 0 if a non-digit is found, 1 otherwise
  */
-int main(int argc, char **argv)
+int is_digit(char *s)
 {
-	char b[15000];
-	char *n = b;
-	int i, j;
+	int i = 0;
 
-	if (argc != 3)
+	while (s[i])
 	{
-		printf("Error\n");
-		exit(98);
+		if (s[i] < '0' || s[i] > '9')
+			return (0);
+		i++;
 	}
-	if (argv[1][0] == '0' || argv[2][0] == '0')
-	{
-		putchar('0');
-		putchar(10);
-		return (0);
-	}
-
-	n = big_mult(argv[1], argv[2]);
-
-	for (i = 0, j = 0;; i++)
-	{
-		if (n[i] != '0')
-			j = 1;
-		if (j == 1 && n[i] == '\0')
-			break;
-		if (j == 1)
-			putchar(n[i]);
-	}
-	putchar(10);
-
-	return (0);
+	return (1);
 }
 
 /**
- * _calloc - Function that allocates memory for an array
- * @nmemb: Elements of array
- * @size: Size of data type
- * Return: Void
- */
-void *_calloc(unsigned int nmemb, unsigned int size)
-{
-	unsigned int i;
-	char *p;
-
-	if (nmemb == 0 || size == 0)
-		return (NULL);
-
-	p = malloc(nmemb * size);
-
-	if (p == NULL)
-		return (NULL);
-
-	for (i = 0; i < nmemb * size; i++)
-		p[i] = 0;
-
-	return (p);
-}
-
-/**
- * _strlen - Function that calculates the length of a string
- * @s: String to be checked
- * Return: The lengtht of string or -1 if it fails
+ * _strlen - returns the length of a string
+ * @s: string to evaluate
+ *
+ * Return: the length of the string
  */
 int _strlen(char *s)
 {
-	int i;
+	int i = 0;
 
-	if (s == NULL)
-		return (-1);
-
-	for (i = 0; s[i]; i++)
-		;
-
+	while (s[i] != '\0')
+	{
+		i++;
+	}
 	return (i);
 }
 
 /**
- * big_mult - Function that multiplies two big numbers
- * @s1: String big number 1
- * @s2: String big number 2
- * Return: a result of the two big numbers
+ * errors - handles errors for main
  */
-char *big_mult(char *s1, char *s2)
+void errors(void)
 {
-	int i, j, k, l, value;
-	char *n;
+	printf("Error\n");
+	exit(98);
+}
 
-	i = _strlen(s1);
-	j = _strlen(s2);
-	k = i + j + 1;
-	n = _calloc(k, sizeof(char));
-	if (n == NULL)
-		printf("Error\n"), exit(98);
-	n[k - 1] = '\0';
+/**
+ * main - multiplies two positive numbers
+ * @argc: number of arguments
+ * @argv: array of arguments
+ *
+ * Return: always 0 (Success)
+ */
+int main(int argc, char *argv[])
+{
+	char *s1, *s2;
+	int len1, len2, len, i, carry, digit1, digit2, *result, a = 0;
 
-	for (--i; i >= 0; i--)
+	s1 = argv[1], s2 = argv[2];
+	if (argc != 3 || !is_digit(s1) || !is_digit(s2))
+		errors();
+	len1 = _strlen(s1);
+	len2 = _strlen(s2);
+	len = len1 + len2 + 1;
+	result = malloc(sizeof(int) * len);
+	if (!result)
+		return (1);
+	for (i = 0; i <= len1 + len2; i++)
+		result[i] = 0;
+	for (len1 = len1 - 1; len1 >= 0; len1--)
 	{
-		if (s1[i] < '0' || s1[i] > '9')
-			free(n), printf("Error\n"), exit(98);
-		for (l = j - 1; l >= 0; l--)
+		digit1 = s1[len1] - '0';
+		carry = 0;
+		for (len2 = _strlen(s2) - 1; len2 >= 0; len2--)
 		{
-			if (s2[l] < '0' || s2[l] > '9')
-				free(n), printf("Error\n"), exit(98);
-			value = (s1[i] - '0') * (s2[l] - '0');
-			n[i + l + 1] += value;
-			if (n[i + l + 1] > 9)
-			{
-				value = n[i + l + 1];
-				n[i + l + 1] %= 10;
-				n[i + l] += value / 10;
-			}
+			digit2 = s2[len2] - '0';
+			carry += result[len1 + len2 + 1] + (digit1 * digit2);
+			result[len1 + len2 + 1] = carry % 10;
+			carry /= 10;
 		}
+		if (carry > 0)
+			result[len1 + len2 + 1] += carry;
 	}
-
-	for (i = 0; i < k - 1; i++)
-		n[i] += '0';
-
-	return (n);
+	for (i = 0; i < len - 1; i++)
+	{
+		if (result[i])
+			a = 1;
+		if (a)
+			_putchar(result[i] + '0');
+	}
+	if (!a)
+		_putchar('0');
+	_putchar('\n');
+	free(result);
+	return (0);
 }
