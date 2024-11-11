d02ff24adb3c871f1ec875ea4c7c6d38359d7473
batrivieredev <baptiste.rivierefr@gmail.com>
2024-11-11 15:10:51 +0100

    101

--- more_malloc_free/101-mul.c
+++ more_malloc_free/101-mul.c
@@ -1,78 +1,127 @@
-#include <stdio.h>
+#include "main.h"
 #include <stdlib.h>
+#include <stdio.h>
 #include <ctype.h>
 
-int is_positive_integer(char *str);
-void print_error_and_exit(const char *message, int status);
-int multiply_numbers(int num1, int num2);
+/**
+ * _is_zero - determines if any number is zero
+ * @argv: argument vector.
+ *
+ * Return: no return.
+ */
+void _is_zero(char *argv[])
+{
+	int i, num1 = 1, num2 = 1;
+
+	for (i = 0; argv[1][i]; i++)
+		if (argv[1][i] != '0')
+		{
+			num1 = 0;
+			break;
+		}
+
+	for (i = 0; argv[2][i]; i++)
+		if (argv[2][i] != '0')
+		{
+			num2 = 0;
+			break;
+		}
+
+	if (num1 == 1 || num2 == 1)
+	{
+		printf("0\n");
+		exit(0);
+	}
+}
 
 /**
- * main - Entry point of the program
- * @argc: Argument count
- * @argv: Argument vector
+ * _initialize_array - set memery to zero in a new array
+ * @ar: char array.
+ * @lar: length of the char array.
  *
- * Return: Always 0 (Success)
+ * Return: pointer of a char array.
+ */
+char *_initialize_array(char *ar, int lar)
+{
+	int i = 0;
+
+	for (i = 0; i < lar; i++)
+		ar[i] = '0';
+	ar[lar] = '\0';
+	return (ar);
+}
+
+/**
+ * _checknum - determines length of the number
+ * and checks if number is in base 10.
+ * @argv: arguments vector.
+ * @n: row of the array.
+ *
+ * Return: length of the number.
+ */
+int _checknum(char *argv[], int n)
+{
+	int ln;
+
+	for (ln = 0; argv[n][ln]; ln++)
+		if (!isdigit(argv[n][ln]))
+		{
+			printf("Error\n");
+			exit(98);
+		}
+
+	return (ln);
+}
+
+/**
+ * main - Entry point.
+ * program that multiplies two positive numbers.
+ * @argc: number of arguments.
+ * @argv: arguments vector.
+ *
+ * Return: 0 - success.
  */
 int main(int argc, char *argv[])
 {
-	int num1, num2, result;
+	int ln1, ln2, lnout, add, addl, i, j, k, ca;
+	char *nout;
 
 	if (argc != 3)
+		printf("Error\n"), exit(98);
+	ln1 = _checknum(argv, 1), ln2 = _checknum(argv, 2);
+	_is_zero(argv), lnout = ln1 + ln2, nout = malloc(lnout + 1);
+	if (nout == NULL)
+		printf("Error\n"), exit(98);
+	nout = _initialize_array(nout, lnout);
+	k = lnout - 1, i = ln1 - 1, j = ln2 - 1, ca = addl = 0;
+	for (; k >= 0; k--, i--)
 	{
-		print_error_and_exit("Error\n", 98);
+		if (i < 0)
+		{
+			if (addl > 0)
+			{
+				add = (nout[k] - '0') + addl;
+				if (add > 9)
+					nout[k - 1] = (add / 10) + '0';
+				nout[k] = (add % 10) + '0';
+			}
+			i = ln1 - 1, j--, addl = 0, ca++, k = lnout - (1 + ca);
+		}
+		if (j < 0)
+		{
+			if (nout[0] != '0')
+				break;
+			lnout--;
+			free(nout), nout = malloc(lnout + 1), nout = _initialize_array(nout, lnout);
+			k = lnout - 1, i = ln1 - 1, j = ln2 - 1, ca = addl = 0;
+		}
+		if (j >= 0)
+		{
+			add = ((argv[1][i] - '0') * (argv[2][j] - '0')) + (nout[k] - '0') + addl;
+			addl = add / 10, nout[k] = (add % 10) + '0';
+		}
 	}
-
-	if (!is_positive_integer(argv[1]) || !is_positive_integer(argv[2]))
-	{
-		print_error_and_exit("Error\n", 98);
-	}
-
-	num1 = atoi(argv[1]);
-	num2 = atoi(argv[2]);
-	result = multiply_numbers(num1, num2);
-
-	printf("%d\n", result);
+	printf("%s\n", nout);
 	return (0);
 }
 
-/**
- * is_positive_integer - Checks if a string represents a positive integer
- * @str: The string to check
- *
- * Return: 1 if the string is a positive integer, 0 otherwise
- */
-int is_positive_integer(char *str)
-{
-	while (*str)
-	{
-		if (!isdigit(*str))
-		{
-			return (0);
-		}
-		str++;
-	}
-	return (1);
-}
-
-/**
- * print_error_and_exit - Prints an error message and exits the program
- * @message: The error message to print
- * @status: The exit status
- */
-void print_error_and_exit(const char *message, int status)
-{
-	printf("%s", message);
-	exit(status);
-}
-
-/**
- * multiply_numbers - Multiplies two integers
- * @num1: The first integer
- * @num2: The second integer
- *
- * Return: The product of num1 and num2
- */
-int multiply_numbers(int num1, int num2)
-{
-	return (num1 * num2);
-}
