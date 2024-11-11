78b0e460cbc707328103aa415f3d0fa61872e613
batrivieredev <baptiste.rivierefr@gmail.com>
2024-11-11 09:20:49 +0100

    101

--- more_malloc_free/101-mul.c
+++ more_malloc_free/101-mul.c
@@ -1,30 +1,53 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include "main.h"
+#include <ctype.h>
+
 /**
-* main - multiply 2 numbers as arguments
-* @argc: number of arguments
-* @argv: arguments
-* Return: 0 success
-*/
+ * _isdigit - checks if a string consists of digits
+ * @str: pointer to the string to check
+ *
+ * Return: 1 if all characters are digits, 0 otherwise
+ */
+int _isdigit(char *str)
+{
+	int i = 0;
+
+	while (str[i])
+	{
+		if (!isdigit(str[i]))
+			return (0);
+		i++;
+	}
+	return (1);
+}
+
+/**
+ * main - multiplies two positive numbers
+ * @argc: argument count
+ * @argv: array of argument strings
+ *
+ * Return: 0 on success, 98 on failure
+ */
 int main(int argc, char *argv[])
 {
-unsigned long mul;
-int i, j;
-	if (argc != 3)
-	{ printf("Error\n");
-	exit(98); }
-	for (i = 1; i < argc; i++)
-	{
-		for (j = 0; argv[i][j] != '\0'; j++)
-		{
-			if (argv[i][j] > 57 || argv[i][j] < 48)
-			{  printf("Error\n");
-			exit(98); }
-		}
+	long num1, num2, result;
 
+	if (argc != 3)
+	{
+		printf("Error\n");
+		exit(98);
 	}
-	mul = atol(argv[1]) *  atol(argv[2]);
-	printf("%lu\n", mul);
-return (0);
+
+	if (!_isdigit(argv[1]) || !_isdigit(argv[2]))
+	{
+		printf("Error\n");
+		exit(98);
+	}
+
+	num1 = atol(argv[1]);
+	num2 = atol(argv[2]);
+	result = num1 * num2;
+
+	printf("%ld\n", result);
+	return (0);
 }
