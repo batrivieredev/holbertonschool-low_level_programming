081bf7d7398de2dbe936fce7839840025d03488e
batrivieredev <baptiste.rivierefr@gmail.com>
2024-11-18 00:34:18 +0100

    0

--- variadic_functions/variadic_functions.h
+++ variadic_functions/variadic_functions.h
@@ -1,20 +1,25 @@
-#ifndef VARIADIC_FUNCTIONS_H
-#define VARIADIC_FUNCTIONS_H
+#ifndef VARIAFUNC
+#define VARIAFUNC
+
 
 #include <stdarg.h>
+#include <stdio.h>
+/**
+ * struct args_t - contain the type and the function.
+ * @format: the format to print
+ * @function: the functions that prints
+ *
+ * Description: contains the type and the format to print.
+ */
+typedef struct args_t
+{
+	char *format;
+	void (*function)();
+} args_t;
 
 int sum_them_all(const unsigned int n, ...);
-
 void print_numbers(const char *separator, const unsigned int n, ...);
-
 void print_strings(const char *separator, const unsigned int n, ...);
+void print_all(const char * const format, ...);
 
-void print_all(const char *const format, ...);
-
-typedef struct printall
-{
-	char *letter;
-	void (*func)(va_list arg);
-} printall_t;
-
-#endif
+#endif /* VARIAFUNC */
