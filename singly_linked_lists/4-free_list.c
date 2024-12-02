01255e1273d7537b30a0bfe78ac7e34a23aa0a9a
batrivieredev <baptiste.rivierefr@gmail.com>
2024-12-02 12:21:51 +0100

    4 peut etre bon

---
+++ singly_linked_lists/4-free_list.c
@@ -0,0 +1,20 @@
+
+/**
+ * free_list - frees memory allocated for a list_t list
+ * @head: reference to beggining of list of list_t nodes
+ *
+ * Return: void
+ */
+void free_list(list_t *head)
+{
+	list_t *current = head;
+	list_t *temp;
+
+	while (current != NULL)
+	{
+		temp = current;
+		current = current->next;
+		free(temp->str);
+		free(temp);
+	}
+}
