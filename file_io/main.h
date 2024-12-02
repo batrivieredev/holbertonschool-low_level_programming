da377848d2071c5b86b59cd7a9cccca0afc61820
batrivieredev <baptiste.rivierefr@gmail.com>
2024-12-02 20:45:24 +0100

    main

--- file_io/main.h
+++ file_io/main.h
@@ -1,21 +1,17 @@
-#ifndef _MAIN_H
-#define _MAIN_H
+#ifndef MAIN_H
+#define MAIN_H
 
-#include <stdio.h>
-#include <stdlib.h>
-
-/* POSIX standards */
-#include <unistd.h>
-
-/* OPEN, READ, WRITE, CLOSE */
 #include <sys/types.h>
 #include <sys/stat.h>
+#include <stdlib.h>
+#include <unistd.h>
 #include <fcntl.h>
+#include <errno.h>
+#include <stdio.h>
 
-int _putchar(char c);
 ssize_t read_textfile(const char *filename, size_t letters);
+
 int create_file(const char *filename, char *text_content);
+
 int append_text_to_file(const char *filename, char *text_content);
-
-
-#endif /* _MAIN_H */
+#endif
